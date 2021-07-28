671cc30de2202e8bb0455de0e04608437d89531b
viratkashyap-835 <73227234+viratkashyap-835@users.noreply.github.com>
2021-07-28 18:28:13 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,50 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank=0;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance,
+      rank:this.rank
+    });
+  }
+  getFinishedPlayer(){
+    var finishedPlayerRef=database.ref('finishedPlayer');
+    finishedPlayerRef.on("value",(data)=>{
+      finishedPlayer=data.val();
+    })
+  }
+
+  static updateFinishedPlayer(){
+    database.ref('/').update({
+      finishedPlayer:finishedPlayer+1
+    })
+    this.rank +=1;
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
